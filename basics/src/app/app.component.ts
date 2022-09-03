def526e839b558c5cf31aff050e4a6e9aad99ad0
Stav Naftali Sananes <77387159+Stav-Sananes@users.noreply.github.com>
2022-09-04 00:41:29 +0300

    Add files via upload

---
+++ basics/src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  name = 'stav sananes';
+  imgURL =
+    'https://picsum.photos/id/237/500/500';
+    images = ['https://picsum.photos/id/237/500/500',]
+  currentDate = new Date();
+  cost = 2000;
+  temperature = 25.3;
+  pizza = {
+    toppings: ['cheese', 'olives'],
+    size: 'large',
+  };
+  blueClass = false;
+  fontSize = 16;
+  getName() {
+    return this.name;
+  }
+  changeImage(e: KeyboardEvent) {
+    this.imgURL = (e.target as HTMLInputElement).value;
+  }
+  logImg(event: string) {
+    console.log(event);
+  }
+}
---
+++ clips/src/app/shared/modal/modal.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { ModalService } from 'src/app/services/modal.service';
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.css'],
+  // providers: [ModalService],
+})
+export class ModalComponent implements OnInit {
+  constructor(public modal: ModalService) {}
+  closeModal() {
+    this.modal.toggleModal();
+  }
+  ngOnInit(): void {}
+}
