48e6a027a589e7537a1f910452e8452f06faf7c8
Stav Sananes <stav.s2020@gmail.com>
2022-09-04 17:44:59 +0300

    New Changes for the app

--- clips/src/app/shared/modal/modal.component.ts
+++ clips/src/app/shared/modal/modal.component.ts
@@ -1,16 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-import { ModalService } from 'src/app/services/modal.service';
+import { Component, Input, OnInit, ElementRef } from "@angular/core";
+import { ModalService } from "src/app/services/modal.service";
 
 @Component({
-  selector: 'app-modal',
-  templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.css'],
+  selector: "app-modal",
+  templateUrl: "./modal.component.html",
+  styleUrls: ["./modal.component.css"],
   // providers: [ModalService],
 })
 export class ModalComponent implements OnInit {
-  constructor(public modal: ModalService) {}
-  closeModal() {
-    this.modal.toggleModal();
+  @Input() modalID = "";
+  constructor(public modal: ModalService, public el: ElementRef) {
   }
-  ngOnInit(): void {}
+  closeModal() {
+    this.modal.toggleModal(this.modalID);
+  }
+  ngOnInit(): void {
+    document.body.appendChild(this.el.nativeElement);
+  }
 }
